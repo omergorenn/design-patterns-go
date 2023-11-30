ea3d0711c276db4999d421cdfacae41baa9b01a6
omergorenn <omergrn25@gmail.com>
2023-11-30 13:22:00 +0300

    "Abstract Factory provides an interface to create objects from each product family, ensuring you don't create incompatible variants."

---
+++ adidas.go
@@ -0,0 +1,22 @@
+package main
+
+type Adidas struct {
+}
+
+func (a *Adidas) makeShoe() IShoe {
+	return &AdidasShoe{
+		Shoe: Shoe{
+			logo: "adidas",
+			size: 43,
+		},
+	}
+}
+
+func (a *Adidas) makeShirt() IShirt {
+	return &AdidasShirt{
+		Shirt: Shirt{
+			logo: "adidas",
+			size: 14,
+		},
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module AbstractFactory-go
+
+go 1.21
